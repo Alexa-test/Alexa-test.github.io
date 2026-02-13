2e0cd45045ce3c477a8f6039aa38d0987457a7a5
Александра Санджиева <73139557+Alexa6748@users.noreply.github.com>
2026-02-13 07:31:00 +0300

    Add 'sport' to ignored icons in click event

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const id = this.dataset.id;
 
             // ←←← ВАЖНО: полностью игнорируем иконку социальных выплат
-            if (id === 'socvyplaty' || id === 'health' ||  id === 'tsr' || id === 'work' || id === 'law' || id == 'edu')) {
+            if (id === 'socvyplaty' || id === 'health' ||  id === 'tsr' || id === 'work' || id === 'law' || id == 'edu' ||id == 'sport')) {
                 return; // ничего не делаем — за это отвечает socvyplaty.js и health.js
             }
 
